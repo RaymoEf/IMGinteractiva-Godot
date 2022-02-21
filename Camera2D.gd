d4c5e51c17c9d360b893d297b24ab8d572ab62d7
Efren Raymundo Morales <efren.raymo@gmail.com>
2022-02-20 20:23:07 -0600

    comentarios

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,12 +1,13 @@
 extends Camera2D
 
-const MOVE_SPEED = 300
+const MOVE_SPEED = 300 #Velovidad de la camara
 
 class_name MainCamera
 
 var _previousPosition: Vector2 = Vector2(0, 0);
 var _moveCamera: bool = false;
 var touch = false
+#variables usadas en modo touch
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -17,7 +18,7 @@ var touch = false
 func _ready():
 	pass # Replace with function body.
 
-func _input(event: InputEvent):
+func _input(event: InputEvent): #movimiento de la camara usando touch por medio de arrastre
 	if touch:
 		if event is InputEventMouseButton && event.button_index == BUTTON_LEFT:
 			get_tree().set_input_as_handled();
@@ -34,8 +35,10 @@ func _input(event: InputEvent):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	if Input.is_action_pressed("ui_up") and position.y > 160:
-		position.y -= MOVE_SPEED*delta	
+	#movimiento de la canmara con botones wasd o flechas de direccion
+	
+	if Input.is_action_pressed("ui_up") and position.y > 160: #si esta presionada la tecla correspondiente y la camara se encuentra dentro del area permitida, esta se mueve
+		position.y -= MOVE_SPEED*delta	#movimiento de la camara, adicionando a la posicion la velocidad multiplicado por delta
 	if Input.is_action_pressed("ui_down") and position.y < get_viewport_rect().size.y-160:
 		position.y += MOVE_SPEED*delta
 	if Input.is_action_pressed("ui_left") and position.x > 256:
@@ -45,17 +48,17 @@ func _process(delta):
 		
 
 
-func _on_Button4_pressed():
+func _on_Button4_pressed(): 
 	touch=true
 	position=Vector2(323,186)
-	zoom=Vector2(0.5,0.5) # Replace with function body.
+	zoom=Vector2(0.5,0.5) 
 
 
-func _on_TouchScreenButton_pressed():
-	if touch:
+func _on_TouchScreenButton_pressed(): #para alternar entre modo touch y modo escitorio
+	if touch: #si esta activo el modo touch, se desactiva y la camara se ajusta al 100%
 		touch=false
 		zoom=Vector2(1,1) 
-	else:
+	else: #Si el modo touch esta inactivo se ajusta la posicion y el xoom (En este modo solo se contempla 50% de zoom)
 		touch=true
 		position=Vector2(323,186)
 		zoom=Vector2(0.5,0.5)
--- Node2D.gd
+++ Node2D.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export(String, FILE, "*.tscn") var target
+export(String, FILE, "*.tscn") var target #variable que guarrda la escena main, para cambiarr de escena
 
 var num = 0
 var com = 0
@@ -16,11 +16,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	num+=1
-	com=num/delta
-	print(com)
 	pass
 
+#Ajustes de zoom, segun el boton seleccionado, se ajusta la posicion y la cantidad de zoom
 
 func _on_Button_pressed():
 	var camera = get_node("Camera2D")
@@ -40,11 +38,11 @@ func _on_Button3_pressed():
 	camera.zoom=Vector2(0.5,0.5)
 
 
-func _on_TouchScreenButton2_pressed():
+func _on_TouchScreenButton2_pressed(): #para cambiar de escena cuando se presiona el boton para ayuda
 	Transitions.fade_out(self, target, 1.5, Color.black)
 
 
-func _on_Button5_pressed():
+func _on_Button5_pressed(): #control de zoom con la combinacion ctrl + rueda del mouse
 	if Input.is_action_pressed("ctrl"):
 		var camera = get_node("Camera2D")
 		var zoom = camera.get_zoom()
